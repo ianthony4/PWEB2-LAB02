6c3bd431ad0a60b5905c0821f7edb0ac3f45045f
ianthony4 <achaisa@unsa.edu.pe>
2023-05-12 11:27:42 -0500

    EJ3 - Definiendo el dia festivo para AQP

--- Ejercicios/Ejercicio3/script.js
+++ Ejercicios/Ejercicio3/script.js
@@ -1,3 +1,9 @@
 let hoy = new Date();
 console.log(hoy);
+//De w3scholls https://www.w3schools.com/jsref/jsref_obj_date.asp
+//Obtenemos los siguientes metodos para un objeto date que se usaran para resolver el problema
+
+//difiniremos el dia festivo
+let diaAqp = new Date(hoy.getFullYear(),15,7);
+console.log(diaAqp);
 
