482dfe51c5aa9fdbbe4105cd398e3fb8b1b40b67
ianthony4 <achaisa@unsa.edu.pe>
2023-05-12 11:42:50 -0500

    Corrigiendo en 'console.log' todo funciona bien

--- Ejercicios/Ejercicio3/script.js
+++ Ejercicios/Ejercicio3/script.js
@@ -8,10 +8,10 @@ let diaAqp = new Date(hoy.getFullYear(),7,15);
 //calculo de los dias que faltan
 //Aqui hay 2 caminos, si la variable 'hoy' es antes del dia festivo y si la misma es despues del dia festivo
 //se diferenciara eso con un if
-if(hoy.getMonth() >= 7 && hoy.getDate()>15){
+if(hoy.getMonth() >= 7 && hoy.getDate() > 15){
 	
 }else{
-		console.log(estamos antes de la fecha);
+		console.log("estamos antes de la fecha");
 }
 console.log(diaAqp);
 
