fed03323fe9257ce67e14a5e9e88c0b64dad43ed
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-12 10:01:09 -0500

    creando variable que recibira la respuesta de la funcion 'invertir'

--- Ejercicios/Ejercicio2/inverso.js
+++ Ejercicios/Ejercicio2/inverso.js
@@ -1,7 +1,10 @@
 //Funcion que muestra la inversa
 function showInv(){
     let elTexto = document.getElementById("texto").value;
-    console.log(elTexto); 
+    console.log(elTexto);
+    let elInverso = invertir(elTexto);  
+    
+
 }
 //Funcion que hara el procesode invertir la palabra o texto, recibe una cadena
 function invertir(text){
