32e07c78a9cbef6bf3dbfa8aada1bff3ef19f005
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-11 23:15:29 -0500

    obteniendo la respuesta al problema, indexando el arreglo

--- Ejercicios/Ejercicio1/scriptFecha.js
+++ Ejercicios/Ejercicio1/scriptFecha.js
@@ -4,5 +4,6 @@ function respuesta(){
     console.log(elDia) //Para ir comprobando en el navegador
     //Se creara un arreglo usando 'const' debido a que solo agregaremos 7 dias
     const semana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
-
+    //ahora obtendremos el dia de la semana 'string' con la variable 'elDia'
+    let rpta = semana[elDia];
 }
