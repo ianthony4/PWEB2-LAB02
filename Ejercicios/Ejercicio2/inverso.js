f9c2eabafef38523716230f16494aae875e2731b
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-12 09:53:25 -0500

    Capturando el texto que ingresa el usuario

--- Ejercicios/Ejercicio2/inverso.js
+++ Ejercicios/Ejercicio2/inverso.js
@@ -1,8 +1,9 @@
 //Funcion que muestra la inversa
 function showInv(){
-
+    let elTexto = document.getElementById("texto").value;
+    console.log(elTexto); 
 }
 //Funcion que hara el procesode invertir la palabra o texto, recibe una cadena
 function invertir(text){
-    
+
 }
