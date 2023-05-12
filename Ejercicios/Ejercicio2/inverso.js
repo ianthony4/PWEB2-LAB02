5efb585ca8912233b9ba9cf1c3440f769ada7218
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-12 10:07:31 -0500

    Se completo la funcion que invierte una palabra (probada en java)

--- Ejercicios/Ejercicio2/inverso.js
+++ Ejercicios/Ejercicio2/inverso.js
@@ -3,10 +3,17 @@ function showInv(){
     let elTexto = document.getElementById("texto").value;
     console.log(elTexto);
     let elInverso = invertir(elTexto);  
-    
+
 
 }
 //Funcion que hara el procesode invertir la palabra o texto, recibe una cadena
 function invertir(text){
-
+    let rpta = "";
+    //Proceso de invertir una palabra
+    for(let i=0;i<=text.length;i++){
+        //incrementamos la variable rpta con 'text' desde atras hacia adelante
+        rpta += text.substr(text.length - i,1);
+    }
+    //retornar la rpta (inverso)
+    return rpta;
 }
