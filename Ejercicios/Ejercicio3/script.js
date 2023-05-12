911a73ee8c48907ca2464b0c0c93ea312fd4d436
ianthony4 <achaisa@unsa.edu.pe>
2023-05-12 11:54:06 -0500

    aplicando la explicacion y comprobando en la consola

--- Ejercicios/Ejercicio3/script.js
+++ Ejercicios/Ejercicio3/script.js
@@ -15,6 +15,6 @@ if(hoy.getMonth() >= 7 && hoy.getDate() > 15){
 //este metodo devuelve la cantidad de milisegundos desde la fecha actual hasta la fecha del 1 de enero de 1970
 //restaremos la fecha del dia festiva - la fecha actual, eso dara como resultados cuandos milisegundos faltan hasta la fecha festiva
 //luego lo convertimos a horas y podemos redondear
-let diasFaltantes = 0;
-console.log(diaAqp);
+let diasFaltantes = diaAqp.getTime() - hoy.getTime();
+console.log(diasFaltantes);
 
