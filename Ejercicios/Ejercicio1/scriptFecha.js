824f66d3add52f47df8bfd8b95e262cad203c452
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-11 23:24:29 -0500

    Ahora se cambiara el documento por la respuesta usando innerHTML

--- Ejercicios/Ejercicio1/scriptFecha.js
+++ Ejercicios/Ejercicio1/scriptFecha.js
@@ -6,4 +6,7 @@ function respuesta(){
     const semana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
     //ahora obtendremos el dia de la semana 'string' con la variable 'elDia'
     let rpta = semana[elDia];
+    console.log(rpta); //para comprobar
+    //Ahora cambiamos el documento html por el valor de 'rpta'
+    document.getElementById("respuesta").innerHTML = rpta;
 }
