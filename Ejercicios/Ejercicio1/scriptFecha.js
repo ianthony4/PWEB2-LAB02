c8ae59d8e3f06e4f88422c49d653254db76075b9
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-11 23:04:42 -0500

    agregando console.log para ir comprobando en consola, esto se hara en varias partes, no se comentara mas al respecto sobre 'console.log'

--- Ejercicios/Ejercicio1/scriptFecha.js
+++ Ejercicios/Ejercicio1/scriptFecha.js
@@ -1,4 +1,5 @@
 function respuesta(){
     //Obtenemos el dia del 'input' donde se entrega el numero
     let elDia = document.getElementById("num").value;
+    console.log(elDia) //Para ir comprobando en el navegador
 }
