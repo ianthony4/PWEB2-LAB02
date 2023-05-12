370d1e8ff4ba215fd1cab351f1c4d20ee0a7a23f
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-11 23:13:48 -0500

    agregando el arreglo con los dias de la semana

--- Ejercicios/Ejercicio1/scriptFecha.js
+++ Ejercicios/Ejercicio1/scriptFecha.js
@@ -2,4 +2,7 @@ function respuesta(){
     //Obtenemos el dia del 'input' donde se entrega el numero
     let elDia = document.getElementById("num").value;
     console.log(elDia) //Para ir comprobando en el navegador
+    //Se creara un arreglo usando 'const' debido a que solo agregaremos 7 dias
+    const semana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+
 }
