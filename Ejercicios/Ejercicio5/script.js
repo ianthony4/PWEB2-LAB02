79e37f7a5ead3f21c7b62c0f56c182ce9e4a1f41
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-14 16:29:26 -0500

    Se crea un arreglo y se procede a crear numeros aleatorios dentro de un bucle for

--- Ejercicios/Ejercicio5/script.js
+++ Ejercicios/Ejercicio5/script.js
@@ -1,4 +1,15 @@
 function crear(){
     let cant = document.getElementById("cantidad").value;
     console.log(cant);
+    //Creando arreglo vacio
+    var valoresTabla = [];
+    //Añadiendo numeros aleatorios del 1 al 100 a cada valor de la tabla
+    //Para crear aleatorio se usa Math.random junto con Math.floor 
+    let aleatorio = 0;
+    for(let i=0;i<cant;i++){
+        //uso de la funcion random
+        aleatorio = Math.floor(Math.random() * 100 ) + 1;
+        
+
+    }
 }
