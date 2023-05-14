79681634ffeaf6352aa0c0ce33771eab8cd498d9
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-14 15:17:43 -0500

    corrigiendo identacion

--- Ejercicios/Ejercicio4/script.js
+++ Ejercicios/Ejercicio4/script.js
@@ -3,20 +3,17 @@ function mostrar(){
     let enlace = document.getElementById("enlace").value;
     console.log(enlace);
     let acortado = acortar(enlace);
-    document.getElementById("respuesta").innerHTML = acortado;
+    document.getElementById("resultado").innerHTML = acortado;
 }
 //En esta funcion procesamos el texto y lo mandamos como respuesta
 function acortar(enlace){
     let aux = "";
     if(enlace.length == 28){
         aux = enlace.slice(16,19)+enlace.slice(20,24)+enlace.slice(25,28);
-        
     }else if(enlace.length == 36){
         aux = enlace.slice(24,27)+enlace.slice(28,32)+enlace.slice(33,36);
-     
     }else{
         aux = "Error, URL no valida";
     }
     return aux;
-
 }
