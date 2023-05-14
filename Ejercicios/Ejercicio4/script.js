c0fccab3f2c735c5898b0587b65998bf1de61be6
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-14 14:46:36 -0500

    Explicando las funciones

--- Ejercicios/Ejercicio4/script.js
+++ Ejercicios/Ejercicio4/script.js
@@ -1,6 +1,8 @@
+//En esta funcion extraemos el texto y la mandamos  'acortar' enviandole el texto
 function mostrar(){
 
 }
+//En esta funcion procesamos el texto y lo mandamos como respuesta
 function acortar(){
-    
+
 }
