f3d7a1d7682050832dedc586f33bc185ea85a200
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-14 14:49:05 -0500

    Extraendo el enlace

--- Ejercicios/Ejercicio4/script.js
+++ Ejercicios/Ejercicio4/script.js
@@ -1,6 +1,7 @@
 //En esta funcion extraemos el texto y la mandamos  'acortar' enviandole el texto
 function mostrar(){
-
+    let enlace = document.getElementById("enlace");
+    console.log(enlace)
 }
 //En esta funcion procesamos el texto y lo mandamos como respuesta
 function acortar(){
